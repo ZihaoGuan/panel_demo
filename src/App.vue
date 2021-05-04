35277e7c22bec536db849e545806b78dab8589ce
zihaoguan <zgua779@aucklanduni.ac.nz>
2021-05-04 16:01:53 +1200

    moved keyframes declaration to global style

--- src/App.vue
+++ src/App.vue
@@ -95,4 +95,33 @@ body {
 .text-center {
   text-align: center;
 }
+
+
+@-moz-keyframes spin {
+  100% {
+    -moz-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+@-webkit-keyframes spin {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+@-o-keyframes spin {
+  100% {
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes spin {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
 </style>
