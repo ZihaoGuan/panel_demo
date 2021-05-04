6e6326627f29578f3b563c0a8f76472be2009088
zihaoguan <zgua779@aucklanduni.ac.nz>
2021-05-04 16:10:11 +1200

    try fix animation issue

--- src/App.vue
+++ src/App.vue
@@ -99,22 +99,19 @@ body {
 
 @-moz-keyframes spin {
   100% {
-    -moz-transform: rotate(360deg);
-    transform: rotate(360deg);
+    -moz-transform: rotate(360deg); 
   }
 }
 
 @-webkit-keyframes spin {
   100% {
     -webkit-transform: rotate(360deg);
-    transform: rotate(360deg);
   }
 }
 
 @-o-keyframes spin {
   100% {
     -o-transform: rotate(360deg);
-    transform: rotate(360deg);
   }
 }
 
